9d5f7bdf01e6709ca0350eceb300d322bb9e0f96
junasrl <muhammadjunas19@gmail.com>
2022-06-10 19:42:45 +0700

    UI dan Game Manager

---
+++ Pong - MDJunas/Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+        Debug.Log("Created by Muhammad Destamal Junas - 149251970100-45");
+    }
+
+    public void OpenAuthor()
+    {
+        Debug.Log("Created by Muhammad Destamal Junas - 149251970100-45");
+    }
+}
